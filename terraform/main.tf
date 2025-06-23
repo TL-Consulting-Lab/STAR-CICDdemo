57c7b501947f89affc869ce460ceccf442dd384e
vanshikaarora230 <vanshikaarora230@gmail.com>
2025-06-24 07:43:15 +1000

    Added Virtual Network, Subnet, and Network Interface configurations

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,70 @@ resource "azurerm_storage_account" "main" {
   }
 }
 
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.resource_group_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
+# Subnet
+resource "azurerm_subnet" "main" {
+  name                 = "${var.resource_group_name}-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Interface
+resource "azurerm_network_interface" "main" {
+  name                = "${var.resource_group_name}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
+// Create a Virtual Machine
+resource "azurerm_linux_virtual_machine" "main" {
+  name                  = "${var.resource_group_name}-vm"
+  resource_group_name   = azurerm_resource_group.main.name
+  location              = azurerm_resource_group.main.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  admin_password        = "P@ssw0rd123!" # Use a secure password in production
+  network_interface_ids = [azurerm_network_interface.main.id]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
 # Outputs
 output "resource_group_id" {
   value = azurerm_resource_group.main.id
