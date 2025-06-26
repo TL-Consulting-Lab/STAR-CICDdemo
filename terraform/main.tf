ebaa55e533c045087c98346985a0e064dfa55870
vanshikaarora230 <vanshikaarora230@gmail.com>
2025-06-27 09:19:27 +1000

    Basic infrastructure setup with resource group and storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,21 @@
-# Resource Group - Main resource group for all Azure resources
+# Resource Group
 resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name  # Name of the resource group
-  location = var.location            # Azure region where resources will be created
+  name     = var.resource_group_name
+  location = var.location
 
   tags = {
-    Environment = var.environment    # Environment tag (dev, staging, prod)
-    ManagedBy   = "Terraform"       # Indicates resource is managed by Terraform
+    Environment = var.environment
+    ManagedBy   = "Terraform"
   }
 }
 
-# Storage Account - For storing application data and backups
+# Storage Account
 resource "azurerm_storage_account" "main" {
-  name                     = lower(replace("${var.resource_group_name}storage", "-", ""))  # Create unique storage account name
-  resource_group_name      = azurerm_resource_group.main.name  # Reference to resource group
-  location                 = azurerm_resource_group.main.location  # Same location as resource group
-  account_tier             = "Standard"  # Performance tier
-  account_replication_type = "LRS"      # Locally redundant storage
+  name                     = lower(replace("${var.resource_group_name}storage", "-", ""))
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
   tags = {
     Environment = var.environment
@@ -23,69 +23,36 @@ resource "azurerm_storage_account" "main" {
   }
 }
 
-# Virtual Machine - Linux VM for application hosting
+// Create a Virtual Machine
 resource "azurerm_linux_virtual_machine" "main" {
-  name                = "${var.resource_group_name}-vm"  # VM name based on resource group
-  resource_group_name = azurerm_resource_group.main.name # Resource group for VM
-  location            = azurerm_resource_group.main.location # Same location as resource group
-  size                = var.vm_size      # VM size/SKU
-  admin_username      = var.admin_username # Administrator username
-  admin_password      = var.admin_password # Administrator password
-  network_interface_ids = [azurerm_network_interface.main.id] # NIC for network connectivity
-
+  name                = "${var.resource_group_name}-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [azurerm_network_interface.main.id] # Assuming a network interface is defined elsewhere  
   os_disk {
-    caching              = "ReadWrite"  # Disk caching type
-    storage_account_type = var.os_disk_type # OS disk storage performance tier
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
-
   source_image_reference {
-    publisher = var.os_publisher # OS image publisher
-    offer     = var.os_offer    # OS image offer
-    sku       = var.os_sku      # OS image SKU
-    version   = var.os_version  # OS image version
-  }
-
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  } 
   tags = {
     Environment = var.environment
     ManagedBy   = "Terraform"
   }
+} 
+
+# Outputs
+output "resource_group_id" {
+  value = azurerm_resource_group.main.id
 }
 
-# Virtual Network - Network infrastructure for VM and other resources
-resource "azurerm_virtual_network" "main" {
-  name                = var.vnet_name  # Name of the virtual network
-  resource_group_name = azurerm_resource_group.main.name # Resource group for VNet
-  location            = azurerm_resource_group.main.location # Same location as resource group
-  address_space       = var.vnet_address_space # VNet IP address space
-
-  tags = {
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
-}
-
-# Subnet - Internal subnet for VM and other resources
-resource "azurerm_subnet" "internal" {
-  name                 = var.subnet_name # Name of the subnet
-  resource_group_name  = azurerm_resource_group.main.name # Resource group for subnet
-  virtual_network_name = azurerm_virtual_network.main.name # Parent virtual network
-  address_prefixes     = var.subnet_address_prefix # Subnet address range
-}
-
-# Network Interface - NIC for VM network connectivity
-resource "azurerm_network_interface" "main" {
-  name                = "${var.resource_group_name}-nic" # NIC name based on resource group
-  resource_group_name = azurerm_resource_group.main.name # Resource group for NIC
-  location            = azurerm_resource_group.main.location # Same location as resource group
-
-  ip_configuration {
-    name                          = "internal" # Name of IP configuration
-    subnet_id                     = azurerm_subnet.internal.id # Reference to subnet
-    private_ip_address_allocation = "Dynamic" # Dynamic IP allocation
-  }
-
-  tags = {
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
+output "storage_account_id" {
+  value = azurerm_storage_account.main.id
 }
